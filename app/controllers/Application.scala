2b6f9c20d0ce5bfce4e42601940708ecbf2b8a64
Markus Klinga <laite@gmx.com>
2015-12-26 11:40:25 +0200

    Move the logic around, separate models to own files

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,63 +1,63 @@
 package controllers
 
+import javax.inject.Inject
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-import play.api.Play.current
+import slick.driver.JdbcProfile
+import slick.driver.PostgresDriver.api._
+import play.api.db.slick.DatabaseConfigProvider
+
 import play.api.libs.json._
 import play.api.mvc._
 
 import models.Message
 import models.Messages
 
+class Application @Inject()(dbConfigProvider: DatabaseConfigProvider) extends Controller {
 
-class Application extends Controller {
+  val dbConfig = dbConfigProvider.get[JdbcProfile]
 
+  val messages = TableQuery[Messages]
+  def findQuery(id: Int) = messages.filter(_.id === id)
 
   def index = Action.async {
-    val msg: Future[Seq[Message]] = Messages.list
-    msg.map(m => Ok(Json.obj("status" -> "Ok", "messages" -> Json.toJson(m))))
+    val message = dbConfig.db.run(messages.result)
+    message.map(msg => Ok(Json.obj("status" -> "Ok", "messages" -> Json.toJson(msg))))
   }
 
   def create = Action.async(BodyParsers.parse.json) { request => 
     val message = request.body.validate[Message]
     message.fold(
-      errors => 
-        Future(BadRequest(Json.obj(
+      errors => Future(BadRequest(Json.obj(
           "status" -> "Parsing message failed",
-          "error" -> JsError.toJson(errors)
-        ))),
+          "error" -> JsError.toJson(errors)))),
       message => 
-        Messages.create(message).map(m =>
-            Ok(Json.obj(
-              "status" -> "Success",
-              "message" -> Json.toJson(m)
-            ))
+        dbConfig.db.run(messages returning messages += message).map(m =>
+            Ok(Json.obj( "status" -> "Success", "message" -> Json.toJson(m)))
         )
     )
   }
 
   def show(id: Int) = Action.async {
-    val message: Future[Message] = Messages.show(id)
+    val message = dbConfig.db.run(findQuery(id).result.head)
     message.map(msg => Ok(Json.obj("status" -> "Ok", "message" -> Json.toJson(msg))))
   }
 
   def update(id: Int) = Action.async(BodyParsers.parse.json) { request =>
     val message = request.body.validate[Message]
     message.fold(
-      errors => 
-        Future(BadRequest(Json.obj(
+      errors => Future(BadRequest(Json.obj(
           "status" -> "Message update failed",
-          "error" -> JsError.toJson(errors)
-        ))),
+          "error" -> JsError.toJson(errors)))),
       message => {
-        Messages.update(id, message)
+        dbConfig.db.run(findQuery(id).update(message))
         Future(Ok(Json.obj("status" -> "Ok", "message" -> Json.toJson(message))))
       }
     )
   }
 
   def delete(id: Int) = Action.async {
-    Messages.delete(id).map(m => Ok(Json.obj("status" -> "Ok")))
+    dbConfig.db.run(findQuery(id).delete).map(m => Ok(Json.obj("status" -> "Ok")))
   }
-}
+} // end controller
--- app/models/Messages.scala
+++ app/models/Messages.scala
@@ -1,30 +1,6 @@
 package models;
 
-import slick.driver.JdbcProfile
 import slick.driver.PostgresDriver.api._
-import play.api.db.slick.DatabaseConfigProvider
-import play.api._
-
-import play.api.libs.json._
-import play.api.libs.functional.syntax._
-
-case class Message (
-  id: Option[Int],
-  message: String
-)
-
-object Message {
-  implicit val messageReads: Reads[Message] = (
-    (JsPath \ "id").readNullable[Int] and
-    (JsPath \ "message").read[String]
-  )(Message.apply _)
-
-
-  implicit val messageWrites: Writes[Message] = (
-    (JsPath \ "id").writeNullable[Int] and
-    (JsPath \ "message").write[String]
-  )(unlift(Message.unapply _))
-}
 
 class Messages (tag: Tag) extends Table[Message](tag, "messages") {
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
@@ -33,33 +9,3 @@ class Messages (tag: Tag) extends Table[Message](tag, "messages") {
   def * = (id.?, message) <> ((Message.apply _).tupled, Message.unapply _)
 }
 
-object Messages {
-
-  val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
-  val messages = TableQuery[Messages]
-
-  def findQuery(id: Int) = {
-    messages.filter(_.id === id)
-  }
-
-  def list = {
-    dbConfig.db.run(messages.result)
-  }
-
-  def create(message: Message) = {
-    dbConfig.db.run(messages returning messages += message)
-  }
-
-  def show(id: Int) = {
-    dbConfig.db.run(findQuery(id).result.head)
-  }
-
-  def update(id: Int, message: Message) = {
-    dbConfig.db.run(findQuery(id).update(message))
-  }
-
-  def delete(id: Int) = {
-    dbConfig.db.run(findQuery(id).delete)
-  }
-
-}
